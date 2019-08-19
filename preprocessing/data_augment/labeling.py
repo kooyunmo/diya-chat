962ee9504c6120d86620510961f6b84cc7545236
KaraxThePhaseSmith <montgomery7289@gmail.com>
2019-08-19 21:38:58 +0900

    add additional explanations for label

--- preprocessing/data_augment/labeling.py
+++ preprocessing/data_augment/labeling.py
@@ -8,11 +8,17 @@ os.chdir("./KoreanChatbot/preprocessing/data_augment/subtitles_html/conv")
 #전처리할 파일 이름 입력 후 파일 읽기
 data_input = input('write file name:')
 data_lines = open(data_input, 'r').readlines()
+#몇번째 문장부터 전처리를 시작할 것인가(쓸데없는 문장들 무시)
+start = int(input('Enter index of sentence to start from:'))
 
-#0과 1을 입력해서 연결 여부를 입력하세요
 df_ = pd.DataFrame(columns = ['sent_1','sent_2','connection'])
-for L in tqdm(range(len(data_lines)-1)):
+for L in tqdm(range(start,len(data_lines)-1)):
     sent1, sent2 = data_lines[L], data_lines[L+1]
-    query = input(sent1+ ' ' + sent2)
+    print('두 문장의 관계는? \n 0: 이어지지 않음, 1:이어짐, 2: 한 문장, 3: 쓸모없음 \n')
+    query = int(input(sent1+ ' ' + sent2))
+    while query != (0 or 1 or 2 or 3):
+        print('0,1,2,3 중 입력해주세요!')
+        query = int(input())
     df_ = df_.append({'sent_1':sent1, 'sent_2':sent2, 'connection':query},ignore_index = True)
-print(df_)
+
+df_.to_pickle("./sent_connection.pkl")
