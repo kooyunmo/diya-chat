adcd514e7e2fab6b5fb5cdc86819cc29c98369c8
kooyunmo <mpbb03@naver.com>
2019-09-20 23:27:09 +0900

    fix comments

--- attention_seq2seq/model.py
+++ attention_seq2seq/model.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+'''
+Teacher forcing is adopted
+    - Teacher forcing is the technique that puts the target token(true label) of the previous step
+      into the next step as an input.
+    - Teacher forcing is activated only at the training stage (not inference stage)
+    - At the inferencing stage, the generated(predicted) token of the previous step is used as a
+      input of next step.
+'''
+
 import tensorflow as tf
 import sys
 import numpy as np
@@ -82,75 +91,78 @@ def Model(features, labels, mode, params):
                                            initializer=embedding_decoder,  # 초기화 값
                                            trainable=False)  # 학습 유무
 
-
-    # 변수 재사용을 위해서 reuse=.AUTO_REUSE를 사용하며 범위를
-    # 정해주고 사용하기 위해 scope설정을 한다.
-    # make_lstm_cell이 "cell"반복적으로 호출 되면서 재사용된다.
+    ################# Encoder ###################
+    # 변수 재사용을 위해서 reuse=tf.AUTO_REUSE로 범위를 정해주고 scope 설정
+    # make_lstm_cell로 만들어진 LSTM cell이 반복적으로 호출 되면서 재사용된다.
     with tf.variable_scope('encoder_scope', reuse=tf.AUTO_REUSE):
-        # 값이 True이면 멀티레이어로 모델을 구성하고 False이면
-        # 단일레이어로 모델을 구성 한다.
+        # True: 멀티레이어로 모델을 구성하고
+        # False: 단일레이어로 모델을 구성 한다.
         if params['multilayer'] == True:
             # layerSize 만큼  LSTMCell을  encoder_cell_list에 담는다.
             encoder_cell_list = [make_lstm_cell(mode, params['hidden_size'], i) for i in range(params['layer_size'])]
-            # MUltiLayer RNN CEll에 encoder_cell_list를 넣어 멀티 레이어를 만든다.
+            # MultiLayerRNNCell에 encoder_cell_list를 넣어 멀티 레이어를 만든다.
             rnn_cell = tf.contrib.rnn.MultiRNNCell(encoder_cell_list, state_is_tuple=False)
+
         else:
             # 단층 LSTMLCell을 만든다.
             rnn_cell = make_lstm_cell(mode, params['hidden_size'], "")
+
         # rnn_cell에 의해 지정된 반복적인 신경망을 만든다.
-        # encoder_outputs(RNN 출력 Tensor)[batch_size,
-        # max_time, cell.output_size]
-        # encoder_states 최종 상태  [batch_size, cell.state_size]
+        # encoder_outputs(RNN 출력 Tensor) = [batch_size, max_time, cell.output_size]
+        # encoder_states 최종 상태 = [batch_size, cell.state_size]
         encoder_outputs, encoder_states = tf.nn.dynamic_rnn(cell=rnn_cell,  # RNN 셀
                                                               inputs=embedding_encoder_batch,  # 입력 값
                                                               dtype=tf.float32)  # 타입
-        # 변수 재사용을 위해서 reuse=.AUTO_REUSE를 사용하며 범위를 정해주고
-        # 사용하기 위해 scope설정을 한다.
-        # make_lstm_cell이 "cell"반복적으로 호출 되면서 재사용된다.
+
+    ################# Decoder ###################
     with tf.variable_scope('decoder_scope', reuse=tf.AUTO_REUSE):
-        # 값이 True이면 멀티레이어로 모델을 구성하고 False이면 단일레이어로
-        # 모델을 구성 한다.
+        # 값이 True이면 멀티레이어로 모델을 구성하고
+        # False이면 단일레이어로 모델을 구성 한다.
         if params['multilayer'] == True:
             # layer_size 만큼  LSTMCell을  decoder_cell_list에 담는다.
             decoder_cell_list = [make_lstm_cell(mode, params['hidden_size'], i) for i in range(params['layer_size'])]
-            # MUltiLayer RNN CEll에 decoder_cell_list를 넣어 멀티 레이어를 만든다.
             rnn_cell = tf.contrib.rnn.MultiRNNCell(decoder_cell_list, state_is_tuple=False)
         else:
             # 단층 LSTMLCell을 만든다.
             rnn_cell = make_lstm_cell(mode, params['hidden_size'], "")
 
-        decoder_state = encoder_states
+        decoder_state = encoder_states      # encoder의 마지막 step에서의 hidden state vector를 decoder의 첫 번째 step의 hidden state로 초기화
+
         # 매 타임 스텝에 나오는 아웃풋을 저장하는 리스트 두개를 만든다.
-        # 하나는 토큰 인덱스는 predict_tokens 저장
-        # 다른 하나는 temp_logits에 logits 저장한다.
+        #   - 1. (predict_tokens) predicted token index를 저장
+        #   - 2. (temp_logits) logits 저장
         predict_tokens = list()
         temp_logits = list()
 
         # 평가인 경우에는 teacher forcing이 되지 않도록 해야한다.
-        # 따라서 학습이 아닌경우에 is_train을 False로 하여 teacher forcing이 되지 않도록 한다.
+        #  => 학습이 아닌 경우에 is_train을 False로 하여 teacher forcing이 되지 않도록 한다.
         output_token = tf.ones(shape=(tf.shape(encoder_outputs)[0],), dtype=tf.int32) * 1
+
         # 전체 문장 길이 만큼 타임 스텝을 돌도록 한다.
         for i in range(DEFINES.max_sequence_length):
-            # 두 번쨰 스텝 이후에는 teacher forcing을 적용하는지 확률에 따라 결정하도록 한다.
-            # teacher forcing rate은 teacher forcing을 어느정도 줄 것인지를 조절한다.
+            # 두 번째 스텝 이후에는 teacher forcing 적용 여부를 확률에 따라 결정
+            #   참고) teacher forcing rate은 teacher forcing을 어느정도 줄 것인지를 조절한다.
             if TRAIN:
                 if i > 0:
-                    # tf.cond를 통해 rnn에 입력할 입력 임베딩 벡터를 결정한다 여기서 true인 경우엔 입력된 output값 아닌경우에는 이전 스텝에
-                    # 나온 output을 사용한다.
+                    # tf.cond를 통해 rnn에 입력할 입력 임베딩 벡터를 결정 (Teacher forcing Y/N)
+                    #   - True: target(true label)의 임베딩 벡터를 넣어줌
+                    #   - False: 이전 step에서 나온(예측된) token의 임베딩 벡터를 넣어줌
                     input_token_emb = tf.cond(
                         tf.logical_and( # 논리 and 연산자
                             True,
                             tf.random_uniform(shape=(), maxval=1) <= params['teacher_forcing_rate'] # 률에 따른 labels값 지원 유무
                         ),
-                        lambda: tf.nn.embedding_lookup(embedding_decoder, labels[:, i-1]),  # labels 정답을 넣어주고 있다.
-                        lambda: tf.nn.embedding_lookup(embedding_decoder, output_token) # 모델이 정답이라고 생각 하는 값
+                        lambda: tf.nn.embedding_lookup(embedding_decoder, labels[:, i-1]),  # True: True label을 입력
+                        lambda: tf.nn.embedding_lookup(embedding_decoder, output_token)     # False: 모델이 예측한 값을 입력
                     )
                 else:
-                    input_token_emb = tf.nn.embedding_lookup(embedding_decoder, output_token) # 모델이 정답이라고 생각 하는 값
-            else: # 평가 및 예측은 여기를 진행해야 한다.
+                    input_token_emb = tf.nn.embedding_lookup(embedding_decoder, output_token) # 모델이 예측한 값
+
+            else:
+                # 학습이 아닌 평가 및 예측에서는 teacher forcing을 사용하면 안 되므로 모델이 예측한 값을 다음 스텝으로 입력
                 input_token_emb = tf.nn.embedding_lookup(embedding_decoder, output_token)
 
-            # 어텐션 적용 부분
+            #################### Applying Attention Mechanism ####################
             if params['attention'] == True:
                 W1 = tf.keras.layers.Dense(params['hidden_size'])
                 W2 = tf.keras.layers.Dense(params['hidden_size'])
@@ -162,16 +174,17 @@ def Model(features, labels, mode, params):
                 # (?, 1, 128) -> (?, 25, 128)
                 hidden_with_time_axis = tf.manip.tile(hidden_with_time_axis, [1, DEFINES.max_sequence_length, 1])
                 # (?, 25, 1)
-                score = V(tf.nn.tanh(W1(encoder_outputs) + hidden_with_time_axis))
+                score = V(tf.nn.tanh(W1(encoder_outputs) + hidden_with_time_axis))      # encoder output vector의 attention score를 구함
                 # score = V(tf.nn.tanh(W1(encoderOutputs) + tf.manip.tile(tf.expand_dims(W2(decoder_state), axis=1), [1, DEFINES.maxSequenceLength, 1])))
                 # (?, 25, 1)
-                attention_weights = tf.nn.softmax(score, axis=-1)
+                attention_weights = tf.nn.softmax(score, axis=-1)       # 어텐션 스코어를 확률값으로 변환
                 # (?, 25, 128)
-                context_vector = attention_weights * encoder_outputs
+                context_vector = attention_weights * encoder_outputs    # 어텐션 스코어(확률)와 encoder output(question의 context 반영)를 곱해서 encoder output의 각 token에 대한 context vector를 구함
                 # (?, 25, 128) -> (?, 128)
-                context_vector = tf.reduce_sum(context_vector, axis=1)
+                context_vector = tf.reduce_sum(context_vector, axis=1)  # 각 token의 context 벡터를 모두 더해서 input sentence의 context 벡터를 구함
                 # (?, 256)
                 input_token_emb = tf.concat([context_vector, input_token_emb], axis=-1)
+
 
             # RNNCell을 호출하여 RNN 스텝 연산을 진행하도록 한다.
             input_token_emb = tf.keras.layers.Dropout(0.5)(input_token_emb)
@@ -189,7 +202,7 @@ def Model(features, labels, mode, params):
             temp_logits.append(output_logits)
 
         # 저장했던 토큰과 logit 리스트를 stack을 통해 메트릭스로 만들어 준다.
-        # 만들게 뙤면 차원이 [시퀀스 X 배치 X 단어 feature 수] 이렇게 되는데
+        # 만들게 되면 차원이 [시퀀스 X 배치 X 단어 feature 수] 이렇게 되는데
         # 이를 transpose하여 [배치 X 시퀀스 X 단어 feature 수] 로 맞춰준다.
         predict = tf.transpose(tf.stack(predict_tokens, axis=0), [1, 0])
         logits = tf.transpose(tf.stack(temp_logits, axis=0), [1, 0, 2])
@@ -207,23 +220,18 @@ def Model(features, labels, mode, params):
             'indexs': predict,  # 시퀀스 마다 예측한 값
             'logits': logits,  # 마지막 결과 값
         }
-        # 에스티메이터에서 리턴하는 값은 tf.estimator.EstimatorSpec
-        # 객체를 리턴 한다.
-        # mode : 에스티메이터가 수행하는 mode (tf.estimator.ModeKeys.PREDICT)
+        # 에스티메이터에서 리턴하는 값은 tf.estimator.EstimatorSpec 객체
+        # mode: 에스티메이터가 수행하는 mode (tf.estimator.ModeKeys.PREDICT)
         # predictions : 예측 값
         if params['serving'] == True:
             return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
 
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
-    # 마지막 결과 값과 정답 값을 비교하는
-    # tf.nn.sparse_softmax_cross_entropy_with_logits(로스함수)를
-    # 통과 시켜 틀린 만큼의
-    # 에러 값을 가져 오고 이것들은 차원 축소를 통해 단일 텐서 값을 반환 한다.
-    # pad의 loss값을 무력화 시킨다. pad가 아닌값은 1 pad인 값은 0을 주어 동작
-    # 하도록 한다.
-    # 정답 차원 변경을 한다. [배치 * max_sequence_length * vocabulary_length]
-    # logits과 같은 차원을 만들기 위함이다.
+    # 마지막 결과 값과 정답 값을 비교하는 tf.nn.sparse_softmax_cross_entropy_with_logits(loss function)를
+    # 통과 시켜 틀린 만큼의 에러 값을 가져 오고 이것들은 차원 축소를 통해 단일 텐서 값을 반환 한다.
+    # pad의 loss값을 무력화 시킨다. PAD가 아닌값은 1 PAD인 값은 0을 주어 동작 하도록 한다.
+    # logits과 같은 차원을 만들기 위해 정답 차원 변경을 한다. [배치 * max_sequence_length * vocabulary_length]
     labels_ = tf.one_hot(labels, params['vocabulary_length'])
 
     if TRAIN and params['loss_mask'] == True:
@@ -234,16 +242,14 @@ def Model(features, labels, mode, params):
         loss = tf.reduce_mean(loss)
     else:
        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels_))
-    # 라벨과 결과가 일치하는지 빈도 계산을 통해
-    # 정확도를 측정하는 방법이다.
-    accuracy = tf.metrics.accuracy(labels=labels, predictions=predict, name='accOp')
 
-    # 정확도를 전체값으로 나눈 값이다.
+
+    accuracy = tf.metrics.accuracy(labels=labels, predictions=predict, name='accOp')
     metrics = {'accuracy': accuracy}
     tf.summary.scalar('accuracy', accuracy[1])
 
-    # 평가 mode 확인 부분이며 평가는 여기 까지
-    # 수행하고 리턴한다.
+
+
     if EVAL:
         # 에스티메이터에서 리턴하는 값은
         # tf.estimator.EstimatorSpec 객체를 리턴 한다.
